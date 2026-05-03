6de2e5c8a262367c47c298810fa6291b5ab4db7c
Gurpreet <ajbagdi6@gmail.com>
2026-05-03 20:55:40 +0530

    msg

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export default function AdminLogin() {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [password, setPassword] = useState("")
 
   const handleSubmit = (e) => {
     e.preventDefault();
