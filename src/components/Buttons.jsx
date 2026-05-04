f196f118b16d0ee4012c373bae5da303a16782c9
Gurpreet <ajbagdi6@gmail.com>
2026-05-04 16:59:38 +0530

    added h1 tag

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Buttons() {
-  return <div>i am feature button</div>;
+  return (
+    <>
+      <div>
+        <div>i am feature button</div>
+        <h1>hello world</h1>
+      </div>
+    </>
+  );
 }
 
 export default Buttons;
